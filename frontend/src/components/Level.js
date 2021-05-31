e153a2386adfd06ea6b3b965287e38d2f4e9053f
szulima <34941973+szulima@users.noreply.github.com>
2021-06-01 00:49:54 +0200

    calculate level

--- frontend/src/components/Level.js
+++ frontend/src/components/Level.js
@@ -5,13 +5,10 @@ export default function Level() {
   const count = useRecoilValue(countState);
 
   const level = (count) => {
-    if (count < 10) return 1;
-    if (count < 20) return 2;
-    if (count < 40) return 3;
-    if (count < 80) return 4;
-    if (count < 130) return 5;
-    if (count < 190) return 6;
-    if (count >= 190) return 7;
+    for (let x = 0; x < count; x++) {
+      const threshold = 2 ** x * 10;
+      if (count < threshold) return x;
+    }
   };
 
   return (
