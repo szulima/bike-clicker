ce18c6dfc872abaa4894e6327ef62d659dc27a61
szulima <34941973+szulima@users.noreply.github.com>
2021-06-07 12:18:07 +0200

    run function setCount each second

--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -1,12 +1,37 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
+import { useEffect, useRef } from "react";
 import { countState } from "../atoms";
 
 export default function Counter() {
-  const count = useRecoilValue(countState);
+  const [count, setCount] = useRecoilState(countState);
+
+  useInterval(() => {
+    // setCount(count + 1);
+  }, 1000);
 
   return (
     <>
       <p>Bikes: {count}</p>
     </>
   );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
